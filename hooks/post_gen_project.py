284a6f4a40790275463f4e348d840fcde3e194a6
Xiang Zheng <zxyful@gmail.com>
2022-02-08 22:40:51 +0800

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,10 +6,21 @@ def remove_open_source_files():
     for file_name in file_names:
         os.remove(file_name)
 
+        
+def git_init():
+    cmd1 = 'git config --local user.name "{{ cookiecutter.author_name }}"'
+    cmd2 = 'git config --local user.email "{{ cookiecutter.email }}"'
+    try:
+        os.system(cmd1)
+        os.system(cmd2)
+    except:
+        pass
+
 
 def main():
     if "{{ cookiecutter.open_source_license }}" == "Not open source":
         remove_open_source_files()
+    git_init()
 
 
 if __name__ == "__main__":
